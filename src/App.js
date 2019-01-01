fb574e539967109502b5830bbcb49c0a2a158546
smckinney <smckinney@pltw.org>
2019-01-01 15:29:39 -0500

    Routing and organization
    Got started with nested routing.
    Organized the components pertaining to "Main" and "Home."
    Styles are huge WIP but not focusing on right now.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import Main from './Main/Main';
+import Home from './Home/Home';
 import './App.css';
 
-// TODO: Notes on using Material Icons, including making them accessible, here:
-// https://material-ui.com/style/icons/#font-icons
+// TODO: There should be a header with a link back to home.
+// TODO: Rename "Main"
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <CssBaseline />
-        <div className="App">
-          <header className="App-header">
-            <Typography component="h2" variant="h1" gutterBottom>Welcome to Project Sunshine</Typography>
-          </header>
-          <section>
-            <Button variant="contained" color="primary">
-              Login
-            </Button>
-          </section>
-          <p className="App-intro">
-            Cupcake ipsum dolor sit. Amet pudding jujubes halvah jelly-o. Cookie croissant sweet jelly-o jelly-o jelly cake jelly lemon drops.
-          </p>
-        </div>
-      </Fragment>
+      <Router>
+        <Fragment>
+          <CssBaseline />
+          <Route exact path="/" component={Main} />
+          <Route path="/home" component={Home} />
+        </Fragment>
+      </Router>
     );
   }
 }
---
+++ src/Main/Main.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import './Main.css';
+
+// TODO: Notes on using Material Icons, including making them accessible, here:
+// https://material-ui.com/style/icons/#font-icons
+const Home = () => (
+  <div className="Main">
+    <header className="Main-header">
+      <Typography component="h2" variant="h1" gutterBottom>Welcome to Project Sunshine</Typography>
+    </header>
+    <section>
+      <Link to="/home">
+        <Button variant="contained" color="primary">
+          Start Packing!
+        </Button>
+      </Link>
+    </section>
+    <p className="Main-intro">
+      Cupcake ipsum dolor sit. Amet pudding jujubes halvah jelly-o. Cookie croissant sweet jelly-o jelly-o jelly cake jelly lemon drops.
+    </p>
+  </div>
+);
+
+export default Home;
